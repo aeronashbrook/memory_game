ebaf72e90f717eb07a47c6d28b573e3361ae1d0c
Aeron Ashbrook <aeronashbrook@Aerons-MBP.frontierlocal.net>
2019-02-12 19:26:17 -0600

    Updates

---
+++ precache-manifest.ad2e6dc00c381c43c8f374d25d6fef84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dd66ad738ff7122f4b0",
+    "url": "/memory_game/static/css/main.9efc9840.chunk.css"
+  },
+  {
+    "revision": "1dd66ad738ff7122f4b0",
+    "url": "/memory_game/static/js/main.1dd66ad7.chunk.js"
+  },
+  {
+    "revision": "3157a09b9f8bc3f76a51",
+    "url": "/memory_game/static/js/runtime~main.3157a09b.js"
+  },
+  {
+    "revision": "ba2021e67c053dfd7e1d",
+    "url": "/memory_game/static/js/2.ba2021e6.chunk.js"
+  },
+  {
+    "revision": "1de93250d571f3f95154f26481ad402a",
+    "url": "/memory_game/index.html"
+  }
+];
